7135cfa22afcdd1edb6f7b05ca66c78147850bd9
Erik Kroon Celander <ek223ur@student.lnu.se>
2023-10-12 14:06:15 +0200

    add basic deque class

---
+++ Deque.java
@@ -0,0 +1,19 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+  private Node<Item> first;
+  private Node<Item> last;
+  private int size;
+
+  private static class Node<Item> {
+    private Item item;
+    private Node<Item> next;
+    private Node<Item> prev;
+  }
+
+  public Iterator<Item> iterator() {
+    return new DequeIterator<Item>(first);
+  }
+}
+
+
