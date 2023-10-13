6a8f932aca23de820e43d9fcd00869b1ed94ba1c
Erik Kroon Celander <ek223ur@student.lnu.se>
2023-10-13 07:46:04 +0200

    add isEmpty()

--- Deque.java
+++ Deque.java
@@ -11,15 +11,21 @@ public class Deque<Item> implements Iterable<Item> {
     private Node<Item> prev;
   }
 
-     public Deque() {
-        first = null;
-        last = null;
-        size = 0;
-    }
+  public Deque() {
+    first = null;
+    last = null;
+    size = 0;
+  }
+
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  public int size() {
+    return size;
+  }
 
   public Iterator<Item> iterator() {
     return new DequeIterator<Item>(first);
   }
 }
-
-
