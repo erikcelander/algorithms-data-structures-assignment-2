f1faf6af733769d8be839d8d329ec49bac584a8c
Erik Kroon Celander <ek223ur@student.lnu.se>
2023-10-26 13:29:06 +0200

    add insert method to hashtable

--- app/src/main/java/uppgift/HashTableQuadraticProbing.java
+++ app/src/main/java/uppgift/HashTableQuadraticProbing.java
@@ -1,26 +1,43 @@
 package uppgift;
 
 public class HashTableQuadraticProbing<K, V> {
-    private static class HashEntry<K, V> {
-        K key;
-        V value;
+  private static class HashEntry<K, V> {
+    K key;
+    V value;
 
-        HashEntry(K key, V value) {
-            this.key = key;
-            this.value = value;
-        }
+    HashEntry(K key, V value) {
+      this.key = key;
+      this.value = value;
+    }
+  }
+
+  private HashEntry<K, V>[] hashTable;
+  private int capacity;
+  private int size;
+
+  public HashTableQuadraticProbing(int capacity) {
+    this.capacity = capacity;
+    this.hashTable = new HashEntry[capacity];
+    this.size = 0;
+  }
+
+  public void put(K key, V value) {
+    int hash = getHash(key);
+    int i = 0;
+    int quadraticHash = hash;
+
+    while (hashTable[quadraticHash] != null && !hashTable[quadraticHash].key.equals(key)) {
+      i++;
+      quadraticHash = (hash + i * i) % capacity;
     }
 
-    private HashEntry<K, V>[] hashTable;
-    private int capacity;
-    private int size;
+    hashTable[quadraticHash] = new HashEntry<>(key, value);
+    size++;
 
-    public HashTableQuadraticProbing(int capacity) {
-        this.capacity = capacity;
-        this.hashTable = new HashEntry[capacity];
-        this.size = 0;
-    }
+  }
 
+  private int getHash(K key) {
+    return key.hashCode() % capacity;
+  }
 
-  
 }
