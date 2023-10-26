10a741b98fa09112089ad8f00c33dd88958d9c57
Erik Kroon Celander <ek223ur@student.lnu.se>
2023-10-26 13:37:52 +0200

    add insert and resize method to hashtable

--- app/src/main/java/uppgift/HashTableQuadraticProbing.java
+++ app/src/main/java/uppgift/HashTableQuadraticProbing.java
@@ -1,5 +1,7 @@
 package uppgift;
 
+import java.util.Arrays;
+
 public class HashTableQuadraticProbing<K, V> {
   private static class HashEntry<K, V> {
     K key;
@@ -15,13 +17,14 @@ public class HashTableQuadraticProbing<K, V> {
   private int capacity;
   private int size;
 
+  @SuppressWarnings("unchecked")
   public HashTableQuadraticProbing(int capacity) {
     this.capacity = capacity;
-    this.hashTable = new HashEntry[capacity];
+    this.hashTable = (HashEntry<K, V>[]) new HashEntry[capacity];
     this.size = 0;
   }
 
-  public void put(K key, V value) {
+  public void insert(K key, V value) {
     int hash = getHash(key);
     int i = 0;
     int quadraticHash = hash;
@@ -34,10 +37,33 @@ public class HashTableQuadraticProbing<K, V> {
     hashTable[quadraticHash] = new HashEntry<>(key, value);
     size++;
 
+    if (size >= capacity / 2) {
+      resize();
+    }
+  }
+
+  private void resize() {
+    capacity = capacity * 2;
+    size = 0;
+
+    // save old hash table
+    HashEntry<K, V>[] oldHashTable = hashTable;
+    // create new hash table with double the capacity
+    hashTable = Arrays.copyOf(oldHashTable, capacity);
+
+    // put all entries from old hash table into new hash table
+    for (HashEntry<K, V> entry : oldHashTable) {
+      if (entry != null) {
+        insert(entry.key, entry.value);
+      }
+    }
   }
 
   private int getHash(K key) {
     return key.hashCode() % capacity;
   }
 
+
+
+  
 }
